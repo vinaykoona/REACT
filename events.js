9a0ddc74398568bc5d909a3263a5d8fd6ea0968e
vinaykoona <vinaykoona77@gmail.com>
2024-11-22 16:26:45 +0530

    Add files via upload

---
+++ events.js
@@ -0,0 +1,14 @@
+<html>
+<body>
+<h2> Enter something here</h2>
+<input type="text" id="text1" onkeydown="keydownevent()"></input>
+<script>
+function keydownevent(){
+document.getElementById("input1"),
+alert("Pressed a key"),
+console.log("function called"),
+
+}
+</script>
+</body>
+</html>
